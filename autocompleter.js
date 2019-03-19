b1d7f29171291c8da9bc73ccf01e254f35619bdd
mkorczynska <k.martyna95@gmail.com>
2019-03-19 23:26:46 +0100

    Changes to be committed: 	modified:   autocompleter.js 	modified:   index.html 	modified:   style.css 	new file:   words.js

--- autocompleter.js
+++ autocompleter.js
@@ -1,105 +1,106 @@
 Vue.component('v-autocompleter', {
-  template: `
-     <div class="autocomplete">
-    <input
-      type="text"
-	  class="search_form"
-	  id="search_form"
-	  v-model="inputContent"
-      @input="change"
-      @keydown.down="down"
-      @keydown.up="up"
-      @keydown.enter="enter"
-    />
-    <ul
-      id="autocomplete-results"
-      v-show="isOpen"
-      class="autocomplete-results"
-    >
-      <li
-        v-for="(result, i) in results"
-        :key="i"
-        @click="setResult(result)"
-        class="autocomplete-result"
-        :class="{ 'is-active': i === arrowCounter }"
-      >
-        {{ result }}
-      </li>
-    </ul>
-  </div>
-  `,
-  props: {
-    items: {
-      type: Array,
-      required: false
+	template: `
+		<div id="ac">
+			<div id="completer">
+				<input
+					type="text"
+					id="search_form"
+					v-model="inputContent"
+					@input="change"
+					@keydown.down="down"
+					@keydown.up="up"
+					@keydown.enter="enter"
+				/>
+			</div>
+			<div v-show="isOpen" class="autocomplete-results">
+				<ul>
+					<li
+						v-for="(result, i) in results"
+						:key="i"
+						@click="setResult(result)"
+						class="autocomplete-result"
+						:class="{ 'is-active': i === counter }"
+					>
+					{{ result }}
+					</li>
+				</ul>
+			</div>
+		</div>
+	`,
+	props: {
+		items: {
+		type: Array,
+		required: false
     }
-  },
+},
 
-  data() {
-    return {
-      isOpen: false,
-      results: [],
-      inputContent: "",
-      arrowCounter: 0,
-    };
-  },
+data() {
+	return {
+	isOpen: false,
+	results: [],
+    inputContent: "",
+    counter: 0,
+	};
+},
 
-  methods: {
-    change() {
-      this.$emit("input", this.inputContent);
-	  this.filterResults();
-      this.isOpen = true;
-	  
-    },
-
+methods: {
+	change() {
+		this.$emit("input", this.inputContent);
+		this.filterResults();
+		this.isOpen = true;
+	},
     filterResults() {
-      this.results = this.items.filter(item => {
+		this.results = this.items.filter(item => {
         return item.toLowerCase().indexOf(this.inputContent.toLowerCase()) ===0;
-      });
+		}).slice(0, 5);
     },
     setResult(result) {
-      this.inputContent = result;
-      this.isOpen = true;
+		this.inputContent = result;
+		this.isOpen = true;
     },
     down(evt) {
-      if (this.arrowCounter < this.results.length) {
-        this.arrowCounter = this.arrowCounter + 1;
-      }
+		if (this.counter < this.results.length) {
+			this.counter = this.counter + 1;
+		}
     },
-    up() {
-      if (this.arrowCounter > 0) {
-        this.arrowCounter = this.arrowCounter - 1;
-      }
+    up(evt) {
+		if (this.counter > 0) {
+			this.counter = this.counter - 1;
+		}
     },
-    enter() {
-      this.inputContent = this.results[this.arrowCounter];
-      this.isOpen = false;
-      this.arrowCounter = -1;
+    enter(evt) {
+		this.inputContent = this.results[this.counter];
+		this.isOpen = false;
+		this.counter = -1;
     },
     handleClickOutside(evt) {
-      if (!this.$el.contains(evt.target)) {
-        this.isOpen = false;
-        this.arrowCounter = -1;
-      }
+		if (!this.$el.contains(evt.target)) {
+			this.isOpen = false;
+			this.counter = -1;
+		}
     }
-  },
-  watch: {
-    items: function(val, oldValue) {
-      if (val.length !== oldValue.length) {
-        this.results = val;
-      }
+},
+
+watch: {
+	items: function(val, oldValue) {
+		if (val.length !== oldValue.length) {
+			this.results = val;
+		}
     }
-  },
-  mounted() {
-    document.addEventListener("click", this.handleClickOutside);
-  },
-  destroyed() {
-    document.removeEventListener("click", this.handleClickOutside);
-  },
+},
+
+mounted() {
+	document.addEventListener("click", this.handleClickOutside);
+},
+
+destroyed() {
+	document.removeEventListener("click", this.handleClickOutside);
+},
+
 })
 
-const app99=new Vue({
-  el: "#app99",
+const vueApp=new Vue({
+  el: "#vueApp",
   data:{
   	  items:animals
   }
